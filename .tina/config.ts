04d04dd244b5c6e7c2b311aca04d25d96892ebdf
Cory Roberts <mail@coryroberts.net>
2023-04-28 08:04:40 -0400

    Teste1243

--- .tina/config.ts
+++ .tina/config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
         ],
         ui: {
           // This is an DEMO router. You can remove this to fit your site
-         // router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+          router: ({ document }) => `/posts/${document._sys.filename}`,
         },
       },
     ],
